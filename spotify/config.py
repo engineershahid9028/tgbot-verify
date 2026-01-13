aa4c7cb4e413572525f722fbf0aaf91a72729767
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-13 16:27:05 +0500

    Update config.py

--- spotify/config.py
+++ spotify/config.py
@@ -8,127 +8,68 @@ MY_SHEERID_URL = 'https://my.sheerid.com'
 # 文件大小限制
 MAX_FILE_SIZE = 1 * 1024 * 1024  # 1MB
 
-# 学校配置 - Pennsylvania State University 多校区
+# School Configuration - Harvard University Campuses
+
 SCHOOLS = {
-    '2565': {
-        'id': 2565,
-        'idExtended': '2565',
-        'name': 'Pennsylvania State University-Main Campus',
-        'city': 'University Park',
-        'state': 'PA',
+    '1001': {
+        'id': 1001,
+        'idExtended': '1001',
+        'name': 'Harvard University - Main Campus',
+        'city': 'Cambridge',
+        'state': 'MA',
         'country': 'US',
         'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.798214,
-        'longitude': -77.85991
+        'domain': 'HARVARD.EDU',
+        'latitude': 42.377003,
+        'longitude': -71.116660
     },
-    '651379': {
-        'id': 651379,
-        'idExtended': '651379',
-        'name': 'Pennsylvania State University-World Campus',
-        'city': 'University Park',
-        'state': 'PA',
+    '1002': {
+        'id': 1002,
+        'idExtended': '1002',
+        'name': 'Harvard Business School',
+        'city': 'Boston',
+        'state': 'MA',
         'country': 'US',
         'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.832783,
-        'longitude': -77.84159
+        'domain': 'HBS.EDU',
+        'latitude': 42.365490,
+        'longitude': -71.122260
     },
-    '8387': {
-        'id': 8387,
-        'idExtended': '8387',
-        'name': 'Pennsylvania State University-Penn State Harrisburg',
-        'city': 'Middletown',
-        'state': 'PA',
+    '1003': {
+        'id': 1003,
+        'idExtended': '1003',
+        'name': 'Harvard Medical School',
+        'city': 'Boston',
+        'state': 'MA',
         'country': 'US',
         'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.204082,
-        'longitude': -76.74168
+        'domain': 'HMS.HARVARD.EDU',
+        'latitude': 42.336590,
+        'longitude': -71.102280
     },
-    '8382': {
-        'id': 8382,
-        'idExtended': '8382',
-        'name': 'Pennsylvania State University-Penn State Altoona',
-        'city': 'Altoona',
-        'state': 'PA',
+    '1004': {
+        'id': 1004,
+        'idExtended': '1004',
+        'name': 'Harvard Law School',
+        'city': 'Cambridge',
+        'state': 'MA',
         'country': 'US',
         'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.54092,
-        'longitude': -78.40825
+        'domain': 'LAW.HARVARD.EDU',
+        'latitude': 42.379620,
+        'longitude': -71.118960
     },
-    '8396': {
-        'id': 8396,
-        'idExtended': '8396',
-        'name': 'Pennsylvania State University-Penn State Berks',
-        'city': 'Reading',
-        'state': 'PA',
+    '1005': {
+        'id': 1005,
+        'idExtended': '1005',
+        'name': 'Harvard Kennedy School',
+        'city': 'Cambridge',
+        'state': 'MA',
         'country': 'US',
         'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.359947,
-        'longitude': -75.97615
-    },
-    '8379': {
-        'id': 8379,
-        'idExtended': '8379',
-        'name': 'Pennsylvania State University-Penn State Brandywine',
-        'city': 'Media',
-        'state': 'PA',
-        'country': 'US',
-        'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 39.92638,
-        'longitude': -75.44698
-    },
-    '2560': {
-        'id': 2560,
-        'idExtended': '2560',
-        'name': 'Pennsylvania State University-College of Medicine',
-        'city': 'Hershey',
-        'state': 'PA',
-        'country': 'US',
-        'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.264244,
-        'longitude': -76.67408
-    },
-    '650600': {
-        'id': 650600,
-        'idExtended': '650600',
-        'name': 'Pennsylvania State University-Penn State Lehigh Valley',
-        'city': 'Center Valley',
-        'state': 'PA',
-        'country': 'US',
-        'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.559208,
-        'longitude': -75.402504
-    },
-    '8388': {
-        'id': 8388,
-        'idExtended': '8388',
-        'name': 'Pennsylvania State University-Penn State Hazleton',
-        'city': 'Hazleton',
-        'state': 'PA',
-        'country': 'US',
-        'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 40.98396,
-        'longitude': -76.03106
-    },
-    '8394': {
-        'id': 8394,
-        'idExtended': '8394',
-        'name': 'Pennsylvania State University-Penn State Worthington Scranton',
-        'city': 'Dunmore',
-        'state': 'PA',
-        'country': 'US',
-        'type': 'UNIVERSITY',
-        'domain': 'PSU.EDU',
-        'latitude': 41.440258,
-        'longitude': -75.62058
+        'domain': 'HKS.HARVARD.EDU',
+        'latitude': 42.371690,
+        'longitude': -71.121060
     }
 }
 
